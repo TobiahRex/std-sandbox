e962ef5ab4e46cd6bb080bce9029a00615026742
Tobiah Rex <tobiah.rex@gmail.com>
2018-05-25 19:19:38 -0700

    writing list of pending tests.

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,8 +3,6 @@ import { readline, composeSolution, lines } from '../solution/';
 
 
 
-describe('Basic Mocha Test', () => {
-  it('2 should equal 2', () => {
-    assert.equal(2, 2);
-  })
+describe('"parseGame" Test', () => {
+  it('Function param should be collection of strings');
 });
