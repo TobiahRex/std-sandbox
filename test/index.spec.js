a790a94104f4a59ca8b200216aec8491eb9884bb
Tobiah Rex <tobiah.rex@gmail.com>
2018-05-25 19:27:13 -0700

    finished describing pending tests.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,25 @@
 import assert from 'assert';
 import { readline, composeSolution, lines } from '../solution/';
 
-
-
-describe('"parseGame" Test', () => {
+describe('Function "parseGame" Test', () => {
   it('Function param should be collection of strings');
+  it('Function output should be collection of objects');
+  it('Each object should have 2 teams and 2 scores');
+  it('Each objects <value> should be a Number');
+});
+describe('Function "getScores" Test', () => {
+  it('Function param should be collection of objects');
+  it('Function output should be single object');
+  it('Output object keys should be strings');
+  it('Output object values should be numbers');
+});
+describe('Function "getWinner" Test', () => {
+  it('Function param should be single Object');
+  it('Funtion output should be single Object');
+});
+
+describe('Function "writeAnswer" Test', () => {
+  it('Function param should be single Object.');
+  it('Funtion output should be single string.');
+  it('Output string should have "Answer" format.')
 });
