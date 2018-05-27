03054d858e94d944d10abc5a1811114072ad5fa7
Tobiah Rex <tobiah.rex@gmail.com>
2018-05-26 17:39:48 -0700

    finished writing tests for "parseGame".

--- test/index.spec.js
+++ test/index.spec.js
@@ -16,29 +16,37 @@ describe('Function "parseGame" Test', () => {
     const results = collection.map((game) => {
       if (typeof game === 'object' && game !== null) return true;
       return false;
-    })
+    });
+
     assert.equal(results.indexOf(false), -1);
   });
 
   it('Each object should have TWO teams and scores.', () => {
-    const objectLength = collection.map((game) => Object.keys(game).length === 2 ? true : false);
+    const objectLength = collection.map((game) =>
+      Object.keys(game).length === 2 ? true : false);
+
     assert.equal(true, objectLength.indexOf(false) === -1);
   });
+
   it('Each objects <value> should be a Number.', () => {
-    const valueTypes = collection.map((results, game) =>
-      Object.keys(game).reduce((team) => {
-        results.push(typeof game[team] === 'number');
+    const valueTypes = collection.map((game) =>
+      Object.keys(game).reduce((results, nextTeam) => {
+        results.push(typeof game[nextTeam].score === 'number');
         return results;
-      }, []));
+      }, []))
+      .reduce((acc, next) => next);
+
     assert.equal(true, valueTypes.indexOf(false) === -1);
   });
 });
+
 describe('Function "getScores" Test', () => {
   it('Function param should be collection of objects.');
   it('Function output should be single object.');
   it('Output object keys should be strings.');
   it('Output object values should be numbers.');
 });
+
 describe('Function "getWinner" Test', () => {
   it('Function param should be single Object.');
   it('Funtion output should be single Object.');
