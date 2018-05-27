5f2c6d4c9afea40e0dcd173d5642dc173d2f219b
Tobiah Rex <tobiah.rex@gmail.com>
2018-05-27 14:35:59 -0700

    writing functional desc for main.js.

--- solution/main.js
+++ solution/main.js
@@ -2,9 +2,22 @@ import rl from 'readline';
 import fs from 'fs';
 import path from 'path';
 import colors from 'colors';
-
 import * as controllers from './';
 
+
+/**
+* Main Control Flow
+* Calls "start" <see "start" func. desc. for more info>,
+* creates a read interface using node's core module "fs" to read scores file line by line.
+* each line s added individually to a collection "lines".
+* "lines", along with the control function "controllers", are passed to a reducer,
+* the reducer, uses functional programming methodology to waterfall the previous functions
+* output into the next functions input, until the final answer is found.
+*
+* @param {object} - Collection of helper functions - for a "pure funciton" pardigm.
+*
+* @return {object} - Collection of helper functions with added file path from user input.
+*/
 controllers
 .start(controllers)
 .then(({
